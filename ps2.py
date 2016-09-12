c4b495c442440f4853cec94a70f3b9a9c37922a7
JTM162092 <akarsh.orai@gmail.com>
2016-09-12 16:39:57 +0530

    finally updated 2

--- ps2.py
+++ ps2.py
@@ -1,3 +1,19 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
+
+import random
+user_count=0  
+user_unit=0     
+
+#number of users
+for user_count in range(1,100):
+	(X,Y)=(random.random()*2- 1, random.random()*2-1)
+	user_count+=1
+#no of user in unit circle	
+	if((X**2+Y**2)<=1):
+		user_unit+=1
+
+
+percentage_user=(float(user_unit)/100)*100
+print "Percentage of users around MSC within unit circle is %d" %percentage_user
