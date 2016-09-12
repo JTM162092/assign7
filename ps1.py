3f27b8a82805cb2d390c50556dc0eedf810612a9
JTM162092 <akarsh.orai@gmail.com>
2016-09-12 16:39:31 +0530

    finally updated1

--- ps1.py
+++ ps1.py
@@ -1,3 +1,34 @@
 ###### this is the first .py file ###########
-
 ####### write your code here ##########
+###Reading inputs and spiliting fields
+inp=raw_input()
+inp2=inp.split( );
+l=len(inp)
+
+#blank string
+default_d={}
+
+
+#crating dictionary
+for i in inp2:
+	p=inp2.count(i)
+	default_d.update({i: p})
+
+
+#sorting the string accroding to values
+out= sorted(default_d.iteritems(), key=lambda x:-x[1])[:3]
+for field in out :
+	print "{0}: {1}".format(*field)
+
+
+
+##permutation program
+
+from itertools import permutations
+for i in inp2:
+	z=len(i)
+	if(z<=1):
+		print i
+	else:
+		iteration = [''.join(pic) for pic in permutations(i)]
+		print iteration[1]
